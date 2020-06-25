49e0fbfad373eba736a2362f00576852e83fbf55
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-25 22:34:41 +0000

    Feature - BaseModel almost completed
    -> constructor [OK]
    -> __str__ [OK]
    -> save [OK]
    -> to_dict [OK]

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+'''
+classes:
+BaseModel: Defines behavior for other sub-classes
+'''
+
+from uuid import uuid4
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    ''' BaseModel Class '''
+
+    def __init__(self, *args, **kwargs):
+        ''' Constructor method '''
+        if (kwargs):
+            for key, value in kwargs.items():
+                if (key == '__class__'):
+                    continue
+                elif (key == 'updated_at' or key == 'created_at'):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
+    def __str__(self):
+        '''Print the BaseModel atrributes.'''
+        return('[' + type(self).__name__ + '] (' + self.id +
+               ') ' + str(self.__dict__))
+
+    def save(self):
+        ''' Update updated_at attr '''
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        ''' Return a dictonary with all key/values '''
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
