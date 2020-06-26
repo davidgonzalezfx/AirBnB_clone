d825aa8101e6a4b79923e9fcedb8a02405307d7d
davidgonzalezfx <dvdramos16@gmail.com>
2020-06-26 08:29:16 +0000

    Feature - Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Amenity Class
+'''
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+    name = ""
