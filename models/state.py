02ff1a0d45418f846e8c0c36ba122c7f7b2ce367
Manga08 <bijuu5445@gmail.com>
2020-06-26 19:08:22 -0500

    [Featured]: The state class

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+State Class
+'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel."""
+    name = ""
